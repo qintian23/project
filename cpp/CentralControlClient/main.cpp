c46d20a3195a9715f2359d8311b07d9e3e90cff3
qintian23 <13927181971@163.com>
2022-12-06 17:00:28 +0800

    更新

--- cpp/CentralControlClient/main.cpp
+++ cpp/CentralControlClient/main.cpp
@@ -6,9 +6,17 @@ int main(int argc, char **argv)
     // 发送心跳
     centralcontrol.RequestClient.SendHeartBeatData();
 
+    byte cmdParam = 0x11;
+    centralcontrol.RequestClient.SendN640PicShow(cmdParam);
+
     // 心跳状态
     bool type = centralcontrol.ResponseClient.HeartBeatResp();
     std::cout << type << std::endl;
+
+    byte input, output;
+    bool falg;
+    centralcontrol.ResponseClient.SignalSwitchResp(&input, &output, &falg);
+    std::cout << input << output << falg  << std::endl;
 
     return 0;
 }
